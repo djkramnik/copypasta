04c5fb148b0dbb30638b4711026bc75822ebabfe
David Gurr <davidk.gurr@gmail.com>
2023-12-06 20:47:31 -0500

    le chien

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,45 @@
 import { 
-  matchUrlToScript
+  matchUrlToScript,
+  ScriptType
 } from './util'
 
 type TabUpdateHandler = (tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => void
+
+const scriptTypeToFunction: Record<ScriptType, (...args: any[]) => void> = {
+  'healthgov': healthGovScript,
+  'caribou': caribouScript,
+}
 
 const handleTabUpdate: TabUpdateHandler = (
   tabId,
   changeInfo,
   tab,
 ) => {
-  // if the changeInfo is complete, match the url to the script and load script if any
-  console.log('changeInfo', tabId, changeInfo)
+  console.log('changeInfo', tabId, changeInfo, tab)
   console.log('matche?', matchUrlToScript(tab.url ?? ''))
-  
+  if (changeInfo.status !== 'complete') {
+    return
+  }
+  // if the changeInfo is "complete", match the url to the script and load script if any
+  const matchedScript = matchUrlToScript(tab.url ?? '')
+  if (!matchedScript) {
+    return
+  }
+  console.log('le chien', scriptTypeToFunction[matchedScript])
+  chrome.scripting.executeScript({
+    target: {
+      tabId,
+    },
+    func: scriptTypeToFunction[matchedScript]
+  })
 }
 
 chrome.tabs.onUpdated.addListener(handleTabUpdate)
 
-function contentScript() {
-  console.log('IM RELOADED!!!')
-  // if the status is 
+function healthGovScript() {
+  console.log('IM RELOADED! health gov edition')
 }
 
+function caribouScript() {
+  console.log('IM RELOADED!!! caribou edition')
+}
